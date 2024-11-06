9734a2aaa2b2a2559231e1dd5d797ea15a760875
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-11-06 17:04:53 +0000

    added varibels

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,5 +6,11 @@ This module is a Micro:bit MicroPython program This program moves pixels
 
 from microbit import *
 
+# Setting the varibles
+
+loopMove = 0
+loopTurn = 0
+
+# setting the screen
 display.clear()
 display.show(Image.HAPPY)
