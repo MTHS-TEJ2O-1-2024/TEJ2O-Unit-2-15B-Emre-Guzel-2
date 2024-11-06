0c6b763b01a76c0c89c8989115fa8f561bb5a32d
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-11-06 13:33:06 +0000

    finished the program

--- main.ts
+++ main.ts
@@ -10,3 +10,31 @@ let sprite: game.LedSprite = null
 //let loopCounter = 0
 let xcnt = 0
 let ycnt = 0
+
+// Setting the screen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Setting the while loop for button a.
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    let cntTurn = 0;
+    //blink on the first led
+    sprite.set(LedSpriteProperty.X, 0)
+    basic.pause(500)
+    //loop for turn
+    while (cntTurn < 4) {
+        let cntMove = 0;
+        //loop for move
+        while (cntMove < 5) {
+            sprite.move(1)
+            cntMove++;
+            basic.pause(500)
+        }
+        sprite.turn(Direction.Right, 90)
+        cntTurn++
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
