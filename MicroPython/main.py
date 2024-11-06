46c446e853b3a53ba2da9334ea3dbcd7f4e810dd
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-11-06 23:13:36 +0000

    added the loops

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,15 +4,14 @@ Created on: Nov 5 2024
 This module is a Micro:bit MicroPython program This program moves pixels
 """
 
-# Liyberies
+# Librarie
 from microbit import *
-import game
-import sprite
 
-# Setting the varibles
+# Setting the variables
 loopMove = 0
 loopTurn = 0
-
+x = 0
+y = 0
 # setting the screen
 display.clear()
 display.show(Image.HAPPY)
@@ -21,9 +20,20 @@ display.show(Image.HAPPY)
 while True:
     if button_a.is_pressed():
         display.clear()
-        # Setting the spirte game
-        sprit = game.create_sprite(0, 0)
+        # It allows the progam to move horizantely
+        x = (x + 1) % 5
         # Reseting the loopTrun
         loopTurn = 0
-        sprite.set(LedSpriteProperty.X, 0)
+        # Setting the pixels of microbit
+        display.set_pixel(x, y, 0)
         sleep(500)
+
+        # Setting the loop for turn
+        while loopTurn < 4:
+            # Reseting the loopMove
+            loopMove = 0
+            # Setting the MoveLopp that way the pixels start to move
+            while loopMove < 5:
+                display.move(1)
+                loopMove +=1
+                sleep(500)
